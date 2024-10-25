ffef17e1275c307eee309c608748547e0ac4a277
khouloudcherrat <96540023+khouloudcherrat@users.noreply.github.com>
2024-10-25 22:03:47 +0200

    Add StudentConfig to initialize sample student data

---
+++ src/main/java/com/sbprojects/demo/student/StudentConfig.java
@@ -0,0 +1,30 @@
+package com.sbprojects.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student student1 = new Student(
+                "khouloud", 
+                "khouloudch@gmail.com", 
+                LocalDate.of(2001, Month.OCTOBER, 15),
+                23
+            );
+            Student student2 = new Student(
+                "meryeme", 
+                "meryemech@gmail.com", 
+                LocalDate.of(1992, Month.JUNE, 26),
+                32
+            );
+            repository.saveAll(List.of(student1, student2));
+        };
+    }
+}

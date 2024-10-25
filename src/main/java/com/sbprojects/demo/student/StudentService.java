f0b539afbbd908156bee0236df463894c44b89a7
khouloudcherrat <96540023+khouloudcherrat@users.noreply.github.com>
2024-10-25 22:03:11 +0200

    configure retrieving students by studentRepository

--- src/main/java/com/sbprojects/demo/student/StudentService.java
+++ src/main/java/com/sbprojects/demo/student/StudentService.java
@@ -1,14 +1,19 @@
 package com.sbprojects.demo.student;
-
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    public List<Student> getStudents(){
-		return List.of(new Student(1, "khouloud", "khouloudch@gmail.com", LocalDate.of(2001, Month.OCTOBER, 15), 23));
+  private final StudentRepository studentRepository;
+  
+  @Autowired
+  public StudentService(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
+  public List<Student> getStudents(){
+		return studentRepository.findAll();
     }
 }
